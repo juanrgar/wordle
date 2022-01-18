1e5a3293e6cf6f0c9355bf23634f103b433806a4
Juan Rafael Garcia Blanco <juanrgar@gmail.com>
2022-01-18 13:55:16 +0100

    Create grid

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 'use strict';
 
-let grid = document.getElementById("grid");
 
+function drawGrid() {
+    let grid = document.getElementById("grid");
+
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement("div");
+        for (let j = 0; j < 5; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            row.append(cell);
+        }
+        grid.append(row);
+    }
+}
+
+drawGrid();
 
